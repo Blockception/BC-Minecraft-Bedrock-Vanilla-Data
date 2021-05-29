a0d9b3fc733ff404b203fc2e77c841e302a34b73
DaanV2 <daanverstraten@hotmail.com>
2021-05-29 11:56:07 +0200

    Added webpacking

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+//@ts-check
+
+var webpack = require("webpack"),
+  path = require("path"),
+  yargs = require("yargs");
+
+var libraryName = "bc-mcbe-vanilla-data",
+  plugins = [],
+  outputFile;
+
+outputFile = "main.js";
+
+/**@type {import('webpack').Configuration}*/
+var config = {
+  entry: [__dirname + "/src/main.ts"],
+  devtool: "source-map",
+  output: {
+    path: path.join(__dirname, "/lib"),
+    filename: outputFile,
+    libraryTarget: "commonjs2",
+    library: libraryName,
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, exclude: /node_modules/, use: [{ loader: "ts-loader" }] },
+      { test: /\.json$/, exclude: /node_modules/, loader: "json-loader", type: "javascript/auto" },
+    ],
+  },
+  resolve: {
+    roots: [path.resolve("./src")],
+    extensions: [".ts", ".js"],
+  },
+  optimization: {
+    minimize: false,
+  },
+  plugins: plugins,
+  target: "node",
+};
+
+module.exports = config;
