6bc43449fb5707c7f7e5b0b0ff50a4be28b84e49
DaanV2 <daanverstraten@hotmail.com>
2021-09-20 11:39:13 +0200

    Fixing blockstate duplicates

--- scraper/Scraper/Classes/GeneralBlock/GeneralBlock - Scrape.cs
+++ scraper/Scraper/Classes/GeneralBlock/GeneralBlock - Scrape.cs
@@ -9,17 +9,24 @@ namespace Scraper {
         public void Scrape(BP.Container Data) {
             foreach (BP.Block Block in Data.Blocks) {
                 foreach (BP.BlockState State in Block.States) {
-                    Int32 Index = this.BlockStates.FindIndex((item) =>item.Name == State.Name && item.Values.Count == State.Values.Count);
+                    Int32 Index = this.BlockStates.FindIndex((item) => item.Name == State.Name);
 
                     if (Index < 0) {
                         this.BlockStates.Add(State);
+                    }
+                    else {
+                        BP.BlockState Item = this.BlockStates[Index];
+
+                        if (State.Values.Count > Item.Values.Count) {
+                            this.BlockStates[Index] = State;
+                        }
                     }
                 }
             }
         }
 
         public void Clean() {
-            this.BlockStates.Sort((x, y)=>x.Name.CompareTo(y.Name));
+            this.BlockStates.Sort((x, y) => x.Name.CompareTo(y.Name));
         }
     }
 }
