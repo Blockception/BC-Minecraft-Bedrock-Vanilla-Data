36099bb06d892d2de9a09172a73520d6e293a636
DaanV2 <daanverstraten@hotmail.com>
2022-09-06 19:41:34 +0200

    Cleaned

--- src/Lib/Types/BehaviorPack/include.ts
+++
@@ -1,8 +0,0 @@
-/*	Auto generated	*/
-
-export * from "./BehaviorPack";
-export * from "./Block";
-export * from "./Entity";
-export * from "./Item";
-export * from "./LootTable";
-export * from "./Trading";
