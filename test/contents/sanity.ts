3cda1c3127934e84770737535641aa046e4745f2
DaanV2 <daanverstraten@hotmail.com>
2024-07-07 11:47:15 +0200

    Updating

--- test/contents/sanity.ts
+++ test/contents/sanity.ts
@@ -17,10 +17,7 @@ function traverse_for_undefined(data: any, path: string): void {
     const item = data[name];
     const t = typeof item;
 
-    expect(t).to.not.equal(
-      undefined,
-      "found an undefined object in object: " + path + " property: " + name
-    );
+    expect(t).to.not.equal(undefined, `found an undefined object in object: ${path} property: ${name}`);
 
     if (t === "object") traverse_for_undefined(item, path + "." + name);
   }
