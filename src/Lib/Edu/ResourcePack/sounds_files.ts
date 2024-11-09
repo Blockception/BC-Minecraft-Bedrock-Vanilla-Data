a613c6ae7349f230d3af88a8d804fbe003332b2b
DaanV2 <daanverstraten@hotmail.com>
2024-11-09 11:17:51 +0100

    updated files

---
+++ src/Lib/Edu/ResourcePack/sounds_files.ts
@@ -0,0 +1,4 @@
+/** Notice: Auto generated file, do not edit */
+
+export const SoundFiles: string[] = 
+[]
--- src/Lib/Vanilla/ResourcePack/animations.ts
+++ src/Lib/Vanilla/ResourcePack/animations.ts
@@ -4220,6 +4220,14 @@ export const Animations: Animation[] =
     "sounds": []
   },
   {
+    "id": "animation.shield.wield_first_person_blocking",
+    "bones": [
+      "shield"
+    ],
+    "particles": [],
+    "sounds": []
+  },
+  {
     "id": "animation.shield.wield_main_hand_first_person",
     "bones": [
       "shield"
@@ -4707,6 +4715,16 @@ export const Animations: Animation[] =
     "sounds": []
   },
   {
+    "id": "animation.vex.attack.rotations.v1.0",
+    "bones": [
+      "body",
+      "leftarm",
+      "rightarm"
+    ],
+    "particles": [],
+    "sounds": []
+  },
+  {
     "id": "animation.vex.charge",
     "bones": [
       "rightarm",
@@ -4724,6 +4742,17 @@ export const Animations: Animation[] =
       "rightwing",
       "rightItem",
       "leftItem"
+    ],
+    "particles": [],
+    "sounds": []
+  },
+  {
+    "id": "animation.vex.move",
+    "bones": [
+      "leftwing",
+      "leg0",
+      "rightarm",
+      "rightwing"
     ],
     "particles": [],
     "sounds": []
